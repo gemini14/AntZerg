97ebee5d44e03f03b6632c9cede63cd629dde815
Tarik Sturm-Dahal <zwillinge14@gmail.com>
2010-12-13 22:30:54 -0500

    Initializer list swap to get rid of warning in Ant class.  Tested build (debug/release) in Eclipse on Ubuntu

--- src/Ants/Ant.cpp
+++ src/Ants/Ant.cpp
@@ -3,9 +3,9 @@
 
 namespace AntZerg
 {
-	Ant::Ant(const std::string& configFile, const std::string& actionScriptFile, const float scalingFactor, 
+	Ant::Ant(const std::string& configFile, const std::string& actionScriptFile, const float scalingFactor,
 		const float x, const float y)
-		: configFile(configFile), actionScript(actionScriptFile), displayScalingFactor(scalingFactor), position(x, y)
+		: position(x, y), displayScalingFactor(scalingFactor), configFile(configFile), actionScript(actionScriptFile)
 	{
 	}
 
